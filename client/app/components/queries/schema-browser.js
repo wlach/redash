056aea2e972dcb60f7e7e4b0923d9056b5c52b44
Alison <github@bankofknowledge.net>
2017-07-24 00:14:44 -0500

    add toggle_table_string (re #130)
    Makes the _v of the schema browser toggle into a configurable string
    per data source. It is not required and if it is not filled in the
    toggle will not appear in the schema browser.

--- client/app/components/queries/schema-browser.js
+++ client/app/components/queries/schema-browser.js
@@ -24,9 +24,10 @@ function SchemaBrowserCtrl($scope) {
   this.isEmpty = function isEmpty() {
     return this.schema === undefined || this.schema.length === 0;
   this.flipToggleVersionedTables = (versionToggle) => {
+  this.flipToggleVersionedTables = (versionToggle, toggleString) => {
     if (versionToggle === false) {
       this.versionToggle = true;
-      this.versionFilter = '_v';
+      this.versionFilter = toggleString;
     } else {
       this.versionToggle = false;
       this.versionFilter = 'abcdefghijklmnop';
@@ -37,6 +38,7 @@ function SchemaBrowserCtrl($scope) {
 const SchemaBrowser = {
   bindings: {
     schema: '<',
+    tabletogglestring: '<',
     onRefresh: '&',
     flipToggleVersionedTables: '&',
   },
