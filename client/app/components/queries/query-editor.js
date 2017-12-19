de662910264e61dc2d5aa64fc175dda167b419ad
Allen Short <ashort@mozilla.com>
2017-12-19 22:09:13 +0000

    Toggle for query editor autocomplete (re #282)

--- client/app/components/queries/query-editor.js
+++ client/app/components/queries/query-editor.js
@@ -117,9 +117,9 @@ function queryEditor(QuerySnippet) {
               if (newSchema !== oldSchema) {
                 const tokensCount =
                   newSchema.reduce((totalLength, table) => totalLength + table.columns.length, 0);
-                // If there are too many tokens we disable live autocomplete,
-                // as it makes typing slower.
-                if (tokensCount > 5000) {
+                // If there are too many tokens or if it's requested via the UI
+                // we disable live autocomplete, as it makes typing slower.
+                if (tokensCount > 5000 || !$scope.$parent.autocompleteQuery) {
                   editor.setOption('enableLiveAutocompletion', false);
                   editor.setOption('enableBasicAutocompletion', false);
                 } else {
@@ -131,6 +131,10 @@ function queryEditor(QuerySnippet) {
 
             $scope.$parent.$on('angular-resizable.resizing', () => {
               editor.resize();
+            });
+            $scope.$parent.$watch('autocompleteQuery', () => {
+              editor.setOption('enableLiveAutocompletion', $scope.$parent.autocompleteQuery);
+              editor.setOption('enableBasicAutocompletion', $scope.$parent.autocompleteQuery);
             });
 
             editor.focus();
