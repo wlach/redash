ce28bb3bdeecb782495131c2610f593f4084ab3e
Jannis Leidel <jannis@leidel.info>
2018-02-14 22:39:49 +0100

    Refactor authentication support for data sources.
    Adds a new BaseHTTPQueryRunner class.

--- redash/query_runner/url.py
+++ redash/query_runner/url.py
@@ -1,41 +1,9 @@
-import requests
-from redash.query_runner import BaseQueryRunner, register
+from redash.query_runner import BaseHTTPQueryRunner, register
 
 
-class Url(BaseQueryRunner):
+class Url(BaseHTTPQueryRunner):
     default_doc_url = ("http://redash.readthedocs.io/en/latest/"
                        "datasources.html#url")
-
-    @classmethod
-    def configuration_schema(cls):
-        return {
-            'type': 'object',
-            'properties': {
-                'url': {
-                    'type': 'string',
-                    'title': 'URL base path'
-                },
-                'username': {
-                    'type': 'string',
-                    'title': 'HTTP username'
-                },
-                'password': {
-                    'type': 'string',
-                    'title': 'HTTP password'
-                },
-                "doc_url": {
-                    "type": "string",
-                    "title": "Documentation URL",
-                    "default": cls.default_doc_url
-                },
-                "toggle_table_string": {
-                    "type": "string",
-                    "title": "Toggle Table String",
-                    "default": "_v",
-                    "info": "This string will be used to toggle visibility of tables in the schema browser when editing a query in order to remove non-useful tables from sight."
-                }
-            }
-        }
 
     @classmethod
     def annotate_query(cls):
@@ -48,7 +16,6 @@ class Url(BaseQueryRunner):
         base_url = self.configuration.get("url", None)
 
         try:
-            error = None
             query = query.strip()
 
             if base_url is not None and base_url != "":
@@ -59,24 +26,18 @@ class Url(BaseQueryRunner):
                 base_url = ""
 
             url = base_url + query
-            auth = None
-            if self.configuration.get("username"):
-                auth = (self.configuration['username'],
-                        self.configuration.get('password', ''))
-            response = requests.get(url, auth=auth)
-            response.raise_for_status()
+
+            response, error = self.get_response(url)
+            if error is not None:
+                return None, error
+
             json_data = response.content.strip()
 
-            if not json_data:
-                error = "Got empty response from '{}'.".format(url)
-
-            return json_data, error
-        except requests.RequestException as e:
-            return None, str(e)
+            if json_data:
+                return json_data, None
+            else:
+                return None, "Got empty response from '{}'.".format(url)
         except KeyboardInterrupt:
-            error = "Query cancelled by user."
-            json_data = None
-
-        return json_data, error
+            return None, "Query cancelled by user."
 
 register(Url)
