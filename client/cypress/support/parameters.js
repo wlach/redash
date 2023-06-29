51b5b2acad8babf729b4e58b8e5d1620b24681a6
Will Lachance <wlachance@voltus.co>
2023-06-29 10:29:50 -0400

    Remove trigger hack

--- client/cypress/support/parameters.js
+++ client/cypress/support/parameters.js
@@ -4,7 +4,6 @@ export function dragParam(paramName, offsetLeft, offsetTop) {
     .trigger("mousedown");
 
   cy.get(".parameter-dragged .drag-handle")
-    .trigger("mousemove", 1, 1, { force: true }) // must have at least 2 mousemove events?
     .trigger("mousemove", offsetLeft, offsetTop, { force: true })
     .trigger("mouseup", { force: true });
 }
